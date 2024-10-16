2435f71fe02c583ebd8ca3025aa0fb40c54b8e6f
emiliavparra <parraemiliavictoria@gmail.com>
2024-10-15 22:23:25 -0300

    Cuarto commit: Clase CatalogoPelicula y modificaciones en clase Pelicula

--- ClaseCatalogoPelicula.py
+++ ClaseCatalogoPelicula.py
@@ -1,45 +1,106 @@
 import os
 
+from ClasePelicula import Pelicula
+
 
 class CatalogoPelicula():  # Defino la clase Catalogo pelicula
     # Inicio el constructor y le doy los dos atributos pedidos
-    def __init__(self, nombre, ruta_archivo):
+    def __init__(self, nombre):
         self.nombre = nombre  # Para que el nombre se guarde en el parametro nombre
         # Para que la ruta del archivo se guarde en ruta_archivo
-        self.ruta_archivo = ruta_archivo
+        self.ruta_archivo = f'Archivos/{self.nombre}.txt'
 
         # Aca capturo una excepcion para que si el catalogo ya existe
-        # cuando se lo intenta crear, siga trabajando sobre el predeterminado
+        # cuando se lo intenta crear, siga trabajando sobre el prepre existente
         try:
             with open(self.ruta_archivo, 'x') as file:
                 print(f'El catalogo {self.nombre} ha sido creado exitosamente')
         except FileExistsError:
             pass
 
-    # Creo el metodo para agregrar peliculas al catalogo:
-    # o el nuevo que se haya creado en el paso anterior
-    # o en el predeterminado (catalogo1)
-    def agregarPelicula(self, nombre, fechaDeEstreno, director):
+    # Creo el metodo para agregrar peliculas al catalogo: Uso la clase Pelicula.
+    def agregarPelicula(self, pelicula):
+        # Lo abro con 'a' para agregar a algo ya existente
         with open(self.ruta_archivo, 'a') as file:
-            file.write(f"'{nombre}', {fechaDeEstreno}, {director}\n")
-        print(f'La pelicula {nombre} se ha agregado con exito al catalogo')
+            # Uso 'str' para convertir la clase pelicula en un string
+            # y que pueda usarse con el metodo __str__ de la clase Pelicula
+            file.write(str(pelicula))
+        # Imprimo un mensaje si fue agregado exitosamente para que el usuario lo sepa
+        print(f'La pelicula {pelicula} ha sido agregada con exito al catalogo {
+              self.ruta_archivo}')
 
     #
-    def listarPeliculas(self):
-        with open(self.ruta_archivo, 'r') as file:
-            print(f'La lista de peliculas del {self.nombre} es: ')
-            listaPeliculas = file.read()
-            print(listaPeliculas)
+    def listarPeliculas(self):  # Metodo para mostrar las peliculas del catalogo
+        # Abro en modo lectura el txt donde se guardan las peliculas
+        # Atrapo una excepcion para el caso de que intente listar un catalogo vacio
+        try:
+            with open(self.ruta_archivo, 'r') as file:
+                # Uso el metodo readlines para leer todas las lineas del archivo a la vez
+                lines = file.readlines()
+                print(f'La lista de peliculas del {self.nombre} es: ')
+                # Recorro cada linea para mostrarlas
+                # Uso el metodo enumerate para obtener un indice para cada pelicula
+                for indice, line in enumerate(lines):
+                    # agrego +1 a indice para que arranque en el indice nro 1 y no en el nro 0 lo que ve el usuario
+                    print(f'{indice+1}. {line}', end='')
+        except:
+            pass
 
-    def eliminarPelicula(self, nombre):
-        with open(self.ruta_archivo, 'r') as file:
-            line = file.readline()
-            while line:
-                print(line.strip())
-                line = file.readline()
+    # Para crear este metodo pensamos que utilizar indices seria la forma mas sencilla y visualmente comoda (igual que listarPeliculas)
+    def eliminarPelicula(self, indicePelicula):
+        # Atrapo una excepcion donde primero leo el archivo con readlines()
+        try:
+            with open(self.ruta_archivo, 'r') as file:
+                lines = file.readlines()
+        # Reescribo el archivo sin la pelicula que se elimina
+            with open(self.ruta_archivo, 'w') as file:
+                for indice, line in enumerate(lines):
+                    # Aqui le pido que si es diferente, reescriba lo que es diferente. Es decir, la elimina reescribiendo las que no elimina
+                    if indice != indicePelicula:
+                        file.write(line)
+        # Este except es para que no rompa el codigo si la pelicula a eliminar no existe
+        except:
+            pass
 
-    def buscarPelicula(self):
-        pass
+    # Defino un metodo para eliminar el catalogo completo
+    def eliminarCatalogo(self):
+        if os.path.exists(self.ruta_archivo):
+            # Le pido la confirmacion al usuario.
+            confirmacion = input(
+                'Estas seguro que queres eliminar el catalogo? [Y/n] :').lower()
+            # Si ingresa algo diferente a n se borrara. Si ingresa incluso n mayuscula, no se borrara
+            if confirmacion != 'n':
+                os.remove(self.ruta_archivo)
+
+    # Defino un metodo para buscar una pelicula por cualquier texto
+    def buscarPelicula(self, busqueda):
+        try:
+            with open(self.ruta_archivo, 'r') as file:
+                # Uso el metodo readlines para leer todas las lineas del archivo a la vez
+                lines = file.readlines()
+                # Recorro cada linea para mostrarlas
+                # Uso el metodo enumerate para obtener un indice para cada pelicula
+                for indice, line in enumerate(lines):
+                    # Uso if para que se muestren todos los resultados que tengan el texto buscado
+                    if busqueda in line:
+                        print(f'{indice+1}. {line}', end='')
+        # Atrapo una excepcion por si la pelicula que busco no existe
+        except:
+            pass
 
 
-Catalogo1 = CatalogoPelicula('Catalogo uno', 'Archivos/catalogo1.txt')
+# # Pruebas del codigo
+# Catalogo1 = CatalogoPelicula('Catalogo 1')
+# Catalogo1.agregarPelicula(Pelicula('Star Wars', 'Ciencia ficcion', 'Juvenil'))
+# Catalogo1.agregarPelicula(Pelicula('Peter Pan', 'Infantil', 'Juvenil'))
+# Catalogo1.agregarPelicula(Pelicula('El Padrino', 'Drama', 'Adulto'))
+
+# Catalogo1.listarPeliculas()
+
+# Catalogo1.eliminarPelicula(0)
+# Catalogo1.listarPeliculas()
+
+# Catalogo1.buscarPelicula('Juvenil')
+
+# Catalogo1.eliminarCatalogo()
+# Catalogo1.listarPeliculas()
--- ClasePelicula.py
+++ ClasePelicula.py
@@ -1,12 +1,25 @@
 # Iniciamos construyendo la Clase Película:
 
 class Pelicula:
-    def __init__(self,nombre,genero,clasificacion):
-        self.__nombre = nombre  # Definimos que el Atributo "Nombre" es privado.
+    def __init__(self, nombre, genero, clasificacion):
+        # Definimos que el Atributo "Nombre" es privado.
+        self.__nombre = nombre
         self.genero = genero
-        self.__clasificacion = clasificacion  # Definimos que el Atributo "Clasificación" es privado.
-    
+        # Definimos que el Atributo "Clasificación" es privado.
+        self.__clasificacion = clasificacion
+
     # Definir el método getter para acceder al nombre de la película.
-    def get_nomnbre(self):
+    def get_nombre(self):
         return self.__nombre
-        
+
+    # Definir el método getter para acceder la clasificacion.
+    def get_clasificacion(self):
+        return self.__clasificacion
+
+    # Uso el metodo __str__ para convertir el objeto en una string y poder utilizarlo en la clase CatalogoPelicula
+    def __str__(self):
+        return (f'{self.__nombre}, {self.genero}, {self.__clasificacion} \n')
+
+# Pruebo el codigo con el metodo __str__ :
+# mi_peli = Pelicula('Hola peli', 'Terror', 18)
+# print(mi_peli)
